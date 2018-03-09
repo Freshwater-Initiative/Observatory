2f2d59d7724f4111105740f61dd1d966a33a9097
lsetiawan <landungs@uw.edu>
2018-03-09 08:39:24 -0800

    Add travis and tests

---
+++ tests/test_ogh.py
@@ -0,0 +1,19 @@
+from __future__ import (absolute_import,
+                        division,
+                        print_function,
+                        unicode_literals)
+
+import os
+import pytest
+
+import ogh
+
+
+# TODO: NEED TESTS!!!
+class Testoghfunctions(object):
+
+    def setup(self):
+        pass
+
+    def test_saveDictOfDf(self):
+        pass
