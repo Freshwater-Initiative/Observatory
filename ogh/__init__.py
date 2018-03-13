ded760dca5b5cf3a52df1ff824909e787a9054ec
lsetiawan <landungs@uw.edu>
2018-03-13 10:54:25 -0700

    cleanup

--- ogh/__init__.py
+++ ogh/__init__.py
@@ -5,11 +5,7 @@ from __future__ import (absolute_import,
 
 from ogh.ogh import *  # noqa
 
-from ._version import get_versions
-
 __author__ = 'Jimmy Phuong'
-__version__ = get_versions()['version']
-del get_versions
 
 from ._version import get_versions
 __version__ = get_versions()['version']
