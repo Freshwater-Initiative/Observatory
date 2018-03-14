6456e098b2c0169c75c7f3a482041cefd1c22891
lsetiawan <landungs@uw.edu>
2018-03-13 19:25:24 -0700

    Fix setup

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     maintainer_email='landungs@uw.edu',
     license='MIT',
     classifiers=[
-        'Development Status :: 1 - Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
